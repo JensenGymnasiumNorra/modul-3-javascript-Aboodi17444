bb50a5cdc78f0ed874f75c256df3e048d8ce5f92
Aboodi17444 <abddow070913@student.jenseneducation.se>
2026-02-10 14:07:20 +0100

    done

--- simple_calculator.js
+++ simple_calculator.js
@@ -1,6 +1,35 @@
-function calculateSum() {
-    let num1 = parseFloat(document.getElementById('num1').value);
-    let num2 = parseFloat(document.getElementById('num2').value);
-    let sum = num1 + num2;
-    document.getElementById('result').innerHTML = 'Result: ' + sum;
+function calculate() {
+  let nums = document.getElementsByClassName("num");
+  let operator = document.getElementById("operator").value;
+
+  if (nums.length < 2) {
+    document.getElementById("result").innerHTML = "Result: Lägg in minst två tal!";
+    return;
+  }
+
+  let result = parseFloat(nums[0].value);
+
+  for (let i = 1; i < nums.length; i++) {
+    let value = parseFloat(nums[i].value);
+
+    if (operator === "+") {
+      result = result + value;
+    } else if (operator === "-") {
+      result = result - value;
+    } else if (operator === "*") {
+      result = result * value;
+    } else if (operator === "/") {
+      result = result / value;
+    }
+  }
+
+  document.getElementById("result").innerHTML = "Result: " + result;
+}
+
+function addInput() {
+  let input = document.createElement("input");
+  input.type = "number";
+  input.value = "0";
+  input.className = "num";
+  document.getElementById("inputs").appendChild(input);
 }
